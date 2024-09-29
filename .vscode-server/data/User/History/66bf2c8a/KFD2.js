b180965bd0c5e39960e2661654409ab0f0742b4c
Richard Haines <richyrich20795@gmail.com>
2024-09-29 16:54:40 -0600

    Delete .vscode-server directory

--- .vscode-server/data/User/History/66bf2c8a/KFD2.js
+++
@@ -1,8 +0,0 @@
-// script.js: BYU IT&C 210a JavaScript
-function on_submit(event) {
-    let formData = new FormData(event.currentTarget);
-    let json = JSON.stringify(Object.fromEntries(formData));
-    alert(json);
-    event.preventDefault();
-}
-// Insert the Lab 1b JavaScript here.
